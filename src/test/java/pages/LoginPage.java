5b80436dfde62f2dc6007ce3bc3d8909cf7ba428
Danil <danilalexeev162@gmail.com>
2026-04-22 16:56:40 +0500

    add homework with PageObject

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+
+    /*
+    1. Описываем элементы для взаимодействия
+    2. Описываем методы взаимодействия с этими элементами
+     */
+
+    private final By USERNAME_FIELD = By.xpath("//*[@data-test='username']");
+    private final By PASSWORD_FIELD = By.xpath("//*[@data-test='password']");
+    private final By LOGIN_BUTTON = By.xpath("//*[@data-test='login-button']");
+    private final By ERROR_MESSAGE = By.xpath("//*[@data-test='error']");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void open() {
+        driver.get("https://saucedemo.com/");
+    }
+
+    public void login(String name, String password) {
+        driver.findElement(USERNAME_FIELD).sendKeys(name);
+        driver.findElement(PASSWORD_FIELD).sendKeys(password);
+        driver.findElement(LOGIN_BUTTON).click();
+    }
+
+    public String getErrorMessage() {
+        return driver.findElement(ERROR_MESSAGE).getText();
+    }
+}
