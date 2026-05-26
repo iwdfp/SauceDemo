e1aec4c9014331645bb0ba5ef26bdac2ffca722c
Danil <danilalexeev162@gmail.com>
2026-05-26 11:19:39 +0500

    add options.addArguments("--headless");

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
 import pages.CartPage;
@@ -40,7 +41,10 @@ public class BaseTest {
             options.addArguments("--headless");
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("--headless");
+            driver = new EdgeDriver(options);
+            driver.manage().window().maximize();
         }
 
         loginPage = new LoginPage(driver);
